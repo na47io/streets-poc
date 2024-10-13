76f31f60c0081cabc1463c7d0642a9ef9f2c9fed
nikolay <alexandroff.nikolay05@gmail.com>
2024-10-13 21:31:42 +0100

    feat: write to db

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { type Route, route, serveDir } from "@std/http";
 
+const kv = await Deno.openKv();
+
 const routes: Route[] = [
   {
     pattern: new URLPattern({ pathname: "/" }),
@@ -27,6 +29,42 @@ const routes: Route[] = [
     pattern: new URLPattern({ pathname: "/static/*" }),
     handler: (req) => serveDir(req),
   },
+  {
+    pattern: new URLPattern({pathname: "/api/vote"}),
+    method: "POST",
+    handler: async (req) => {
+
+      const {name, vote} = await req.json();
+
+      // need to store each vote as an individual row in the database
+      const key = vote == "hot" ? ["votes", "hot", name] : ["votes", "not", name]
+
+      await kv
+        .atomic()
+        .mutate({
+          type:"sum",
+          key,
+          value: new Deno.KvU64(1n)
+        })
+        .commit()
+
+      return new Response("Vote cast", { status: 201 });
+    }
+  },
+  {
+    pattern: new URLPattern({pathname: "/api/vote"}),
+    method: "GET",
+    handler: async (req) => {
+      const iter = kv.list<string>({prefix: ["votes"]});
+      const votes = [];
+      for await (const res of iter) votes.push(res);
+
+      console.log(votes)
+      return new Response(JSON.stringify(votes, (k,v) => 
+        typeof v === "bigint" ? Number(v) : v
+    ), { status: 200 });
+    }
+  }
 ];
 
 function defaultHandler(_req: Request) {
